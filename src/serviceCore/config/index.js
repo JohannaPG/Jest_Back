5a8eca308f2871ec6f321adf6a6c4ccac86fa12b
JPG-QA <johannapalominogamarra@gmail.com>
2020-12-31 11:38:57 -0500

    test

--- src/serviceCore/config/index.js
+++
@@ -1,3 +0,0 @@
-export default {
-  BASE_URL: process.env.PAOServiceCore_URL
-};
--- src/user/config/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  BD_HOST: process.env.BD_HOST,
-  BD_NAME: process.env.BD_USER_NAME,
-  BD_USER: process.env.BD_USER,
-  BD_PASSWORD: process.env.BD_PASSWORD,
-  BASE_URL: process.env.USER_URL
-};
